67222679841da16da7f8c4325e579bc8966b64c4
rafael.machado@solides.com.br <rafael.machado@solides.com.br>
2020-10-21 21:20:22 -0300

    Criacao classe Cliente

--- cliente.py
+++ cliente.py
@@ -3,97 +3,8 @@ from threading import Thread
 import tkinter
 import re
 import json
-import sqlite3
 
 c=0
-db=sqlite3.connect('lab_redes.db')
-
-def createTable():
-    try:
-        cur=db.cursor()
-        cur.execute('''CREATE TABLE users (
-                       UserID INTEGER PRIMARY KEY AUTOINCREMENT,
-                       name TEXT (20) NOT NULL,
-                       password TEXT (20) NOT NULL
-                       );''')
-        print ('Tabela criada com sucesso')
-    except:
-        db.rollback()
-
-def login(name,password):
-    qry="SELECT from users where name=?, password=?;"
-    try:
-        cur=db.cursor()
-        cur.execute(qry, (name,password))
-        print(cur.fetchone())
-        return True
-    except:
-        db.rollback()
-        return False
-
-def userList():
-    try:
-        cur=db.cursor()
-        cur.execute("SELECT name FROM users;")
-        print(cur.fetchone())
-    except:
-        db.rollback()
-        return False
-
-def createUser(name,password):
-    try:
-        cur=db.cursor()
-        cur.execute("insert into users (name, password) values(?, ?);", (name,password))
-        db.commit()
-        print ("Registrado com sucesso")
-    except:
-        print("Erro na execução")
-        db.rollback()
-
-def updateUsername(current_name,name):
-    qry="update users set name=?, where name=?;"
-    try:
-        cur=db.cursor()
-        cur.execute(qry, (name,current_name))
-        db.commit()
-        print("Registro atualizado com sucesso")
-    except:
-        print("Erro na execução")
-        db.rollback()
-
-def updateUserpassword(name,password):
-    qry="update users set password=? where name=?;"
-    try:
-        cur=db.cursor()
-        cur.execute(qry, (password,name))
-        db.commit()
-        print("Registro atualizado com sucesso")
-    except:
-        print("Erro na execução")
-        db.rollback()
-
-def deleteUser(name):
-    print(name)
-    qry="DELETE from users where name=?;"
-    try:
-        cur=db.cursor()
-        cur.execute(qry, (name))
-        db.commit()
-        print("Registro excluído com sucesso")
-    except:
-        print("Erro na execução")
-        db.rollback()
-
-def deleteTable():   
-    try:
-        cur=db.cursor()
-        cur.execute('DROP TABLE users;')
-        db.commit()
-        print("Tabela excluída com sucesso")
-    except:
-        print("Erro na execução")
-        db.rollback()
-        db.close()
 
 def receive():
     global c;
@@ -104,11 +15,11 @@ def receive():
             if msg == 'Nome em uso!':
                 c+=-1 
             msg_list.insert(tkinter.END, msg)
-        except OSError:  # Cliente deixou o chat
+        except OSError:  # Possibly client has left the chat.
             break
 
-def send(event=None): 
-    # Gerencia o envio de mensagens
+def send(event=None):  # event is passed by binders.
+    #Handles sending of messages.
     global c;
     # print(c)
     if c > 0:
@@ -124,7 +35,7 @@ def send(event=None):
         msg = re.sub(pattern, '', msg)
         msg = msg.strip() 
         dic['mensagem'] = msg 
-        my_msg.set("") # Limpa o campo de enviar texto
+        my_msg.set("")  # Clears input field.
     
         msg = json.dumps(dic)
         client_socket.send(bytes(msg, "utf8"))
@@ -132,74 +43,31 @@ def send(event=None):
         if name == "quit":
             client_socket.close()
             top.quit()
-            print('Conexão Encerrada')
+            print('connection closed')
     else:
         msg = my_msg.get()
-        my_msg.set("")  # Limpa o campo de enviar texto
+        my_msg.set("")  # Clears input field.
         client_socket.send(bytes(msg, "utf8"))
         if msg == "{quit}":
             client_socket.close()
             top.quit()
-            print('Conexão Encerrada')
+            print('connection closed')
         c=+1
-
+    
 
 def on_closing(event=None):
-    # Fechar conexão quando a janela é fechada
+    #This function is to be called when the window is closed.
     my_msg.set("{quit}")
     send()
-    print("Conexão Encerrada")
-
-def menu():
-    print("Escolha uma das opções:\n1 - Login\n2 - Cadastro\n3 - Opções")
-    option = input()
-
-    if option == '1':
-        name = input('Insira seu nome:\n')
-        password = input('Insira sua senha:\n')
-        login(name,password)
-        return name
-    elif option == '2':
-        name = input('Insira seu nome:\n')
-        password = input('Insira sua senha:\n')
-        createUser(name,password)
-        return name
-    else:
-        print("1 - Atualizar nome\n2 - Atualizar senha\n3 - Excluir Usuário\n4 - Listar Usuários")
-        option = input()
-        if option == '1':
-            current_name = input('Insira seu nome atual:\n')
-            name = input('Insira o novo nome:\n')
-            updateUsername(current_name,name)
-            menu()
-        elif option == '2':
-            name = input('Insira seu nome:\n')
-            password = input('Insira a nova senha:\n')
-            updateUserpassword(name,password)
-            menu()
-        elif option == '3':
-            name = input('Insira seu nome:\n')    
-            deleteUser(name)
-            menu()
-        else:
-            userList()
-            menu()    
-        
+    print("connection closed")
 
 if __name__ == "__main__":
-
-    createTable()
-    HOST = ''
-    PORT = 33000
-    print("Bem-Vindo!")
-    usuario = menu()
-
     top = tkinter.Tk()
-    top.title("Bate-Papo")
+    top.title("Chat In a Box")
     messages_frame = tkinter.Frame(top)
-    my_msg = tkinter.StringVar()  # Para as mensagens a serem enviadas.
-    my_msg.set(usuario)
-    scrollbar = tkinter.Scrollbar(messages_frame)  # Para navegação entre as mensagens.
+    my_msg = tkinter.StringVar()  # For the messages to be sent.
+    my_msg.set("Type your messages here.")
+    scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
     msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
     scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
     msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
@@ -208,9 +76,20 @@ if __name__ == "__main__":
     entry_field = tkinter.Entry(top, textvariable=my_msg)
     entry_field.bind("<Return>", send)
     entry_field.pack()
-    send_button = tkinter.Button(top, text="Enviar", command=send)
+    send_button = tkinter.Button(top, text="Send", command=send)
+    send_button.pack()
+    # button to send a delete query
+    send_button = tkinter.Button(top, text="Sakujo username ", command=send)
     send_button.pack()
     top.protocol("WM_DELETE_WINDOW", on_closing)
+
+    HOST = input('Enter host: ')
+    PORT = input('Enter port: ')
+
+    if not PORT:
+        PORT = 5000  # Default value.
+    else:
+        PORT = int(PORT)
 
     BUFSIZ = 1024
     ADDR = (HOST, PORT)
@@ -219,4 +98,5 @@ if __name__ == "__main__":
 
     receive_thread = Thread(target=receive)
     receive_thread.start()
-    tkinter.mainloop()
+    tkinter.mainloop()  # Starts GUI execution.
+
